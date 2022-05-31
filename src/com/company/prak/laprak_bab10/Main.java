7c6f4f00700abb4d00cc88032fb1ed66a914b4e4
hasfi-therasyan <hasfirasya02@gmail.com>
2022-05-31 13:40:26 +0700

    Update Main.java

--- src/com/company/prak/laprak_bab10/Main.java
+++ src/com/company/prak/laprak_bab10/Main.java
@@ -2,5 +2,10 @@ package com.company.prak.laprak_bab10;
 
 public class Main {
     public static void main(String[] args) {
+/*        new JavaString();
+        new JavaStringBuffer();
+        new JavaStringBuilder();
+        new JavaStringJoiner();*/
+        new JavaStringTokenizer();
     }
 }
